65a329b88e9dc93d7e49723262d50ebf6c246d32
Simon Chen <chenxu@research.att.com>
2012-02-21 13:45:36 +0000

    scripts

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 tar zxvf ixgbe-3.7.17.tar.gz
 cd ixgbe-3.7.17/src
-apt-get install build-essential
+apt-get install build-essential lldpd
 make install
 rmmod ixgbe
 modprobe ixgbe
@@ -10,5 +10,14 @@ cat bond0.conf >> /etc/network/interfaces
 ifconfig eth2 0.0.0.0 up
 ifconfig eth3 0.0.0.0 up
 ifup bond0
-ifdown bond0
+
+echo "$1" > /etc/hostname
+
+echo "auto bond0.4051" >> /etc/network/interfaces
+echo "iface bond0.4051 inet static" >> /etc/network/interfaces
+echo "  address $2" >> /etc/network/interfaces
+echo "  netmask 255.255.255.0" >> /etc/network/interfaces
+echo "  vlan_raw_device bond0" >> /etc/network/interfaces
+
+
 
